87fccf6f4c77d2449cf978d36754ae1806d05fa1
nickm0rash <71356183+nickm0rash@users.noreply.github.com>
2021-10-26 00:37:46 -0300

    Add files via upload

---
+++ include/output.h
@@ -0,0 +1,11 @@
+//output.h
+
+//#pragma once
+#ifndef __OUTPUT__
+#define __OUTPUT__
+
+class Output {
+		public:
+				void image(int width, int height);
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "include/output.h"
+#include "include/vecmath.h"
+
+using namespace std;
+
+int main() {
+        Output o;
+        o.image(1000, 1000);
+}	
